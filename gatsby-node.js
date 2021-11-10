b478dba83fba7a6200ced511160b780d82e2427b
Dania <o.dania@icloud.com>
2021-11-10 21:50:45 +0100

    initiating a second build

--- gatsby-node.js
+++ gatsby-node.js
@@ -30,7 +30,7 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
 
   if (result.errors) {
     reporter.panicOnBuild(
-      `There was an error loading your blog posts`,
+      `There was an error loading your blog posts!`,
       result.errors
     )
     return
